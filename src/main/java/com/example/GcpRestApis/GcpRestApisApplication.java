645fccd2d4f10b16a79e651e14b5f6e36bad151e
Akshaysingh Yaduvanshi <ayaduvanshi@paloalotnetworks.com>
2021-03-15 22:09:23 +0530

    Add code list vm instance

--- src/main/java/com/example/GcpRestApis/GcpRestApisApplication.java
+++ src/main/java/com/example/GcpRestApis/GcpRestApisApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 @SpringBootApplication
 public class GcpRestApisApplication implements CommandLineRunner {
 
+	@Autowired private VmInstanceFetcher vmInstanceFetcher;
 
 
 	public static void main(String[] args) {
@@ -17,23 +18,7 @@ public class GcpRestApisApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//Creating a HttpClient object
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		//Creating a HttpGet object
-		HttpGet httpget = new HttpGet("https://www.tutorialspoint.com/ ");
-		//Printing the method used
-		System.out.println("Request Type: "+httpget.getMethod());
-		//Executing the Get request
-		HttpResponse httpresponse = httpclient.execute(httpget);
-		Scanner sc = new Scanner(httpresponse.getEntity().getContent());
-		//Printing the status line
-		System.out.println(httpresponse.getStatusLine());
-		while(sc.hasNext()) {
-			System.out.println(sc.nextLine());
-		}
+		vmInstanceFetcher.listVmInstances("durable-trainer-251010", "us-central1-c" );
 	}
-
-
-
 
 }
---
+++ src/main/java/com/example/GcpRestApis/VmInstanceFetcher.java
@@ -0,0 +1,49 @@
+package com.example.GcpRestApis;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.compute.Compute;
+import com.google.api.services.compute.model.Instance;
+import com.google.api.services.compute.model.InstanceList;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VmInstanceFetcher {
+   public void listVmInstances(String project, String zone) throws IOException, GeneralSecurityException {
+      Compute computeService = createComputeService();
+      Compute.Instances.List request = computeService.instances().list(project, zone);
+
+      InstanceList response;
+      do {
+        response = request.execute();
+        if (response.getItems() == null) {
+          continue;
+        }
+        for (Instance instance : response.getItems()) {
+          // TODO: Change code below to process each `instance` resource:
+          System.out.println(instance);
+        }
+        request.setPageToken(response.getNextPageToken());
+      } while (response.getNextPageToken() != null);
+    }
+
+    public static Compute createComputeService() throws IOException, GeneralSecurityException {
+      HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+      JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+      GoogleCredential credential = GoogleCredential.getApplicationDefault();
+      if (credential.createScopedRequired()) {
+        credential =
+            credential.createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform"));
+      }
+
+      return new Compute.Builder(httpTransport, jsonFactory, credential)
+          .setApplicationName("Google-ComputeSample/0.1")
+          .build();
+    }
+}
